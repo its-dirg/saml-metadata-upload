6da78f3ec995df155781bb8eaa6e7ca2537d6fc6
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2015-11-21 05:46:52 +0100

    Moved all dependencies to setup.py.

--- setup.py
+++ setup.py
@@ -15,5 +15,12 @@ setup(
     license='Apache License 2.0',
     author='Rebecka Gulliksson',
     author_email='rebecka.gulliksson@umu.se',
-    description='Simple file upload service for SAML metadata.'
+    description='Simple file upload service for SAML metadata.',
+    install_requires=[
+        'Flask==0.10.1',
+        'flask-transfer==0.0.1',
+        'Flask-WTF==0.12',
+        'Flask-Bootstrap==3.3.5.7',
+        'lxml==3.4.4'
+    ]
 )
